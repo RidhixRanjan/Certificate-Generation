059a98fc77adb2296d6d6b56528e12cf02362388
RidhixRanjan <91353795+RidhixRanjan@users.noreply.github.com>
2023-09-02 22:52:56 +0530

    Update kartexa.js

--- kartexa.js
+++ kartexa.js
@@ -1,43 +1,44 @@
- 
-
+// Select DOM elements
 const imgDiv = document.querySelector('.profile-pic-div');
-const img = document.querySelector('#photo');
-const file = document.querySelector('#file');
-const uploadBtn = document.querySelector('#uploadBtn');
+const img = document.querySelector('.photo');
+const file = document.querySelector('.file');
+const uploadBtn = document.querySelector('.uploadBtn');
 
- 
-
-imgDiv.addEventListener('mouseenter', function(){
+// Show the upload button when hovering over the profile picture
+imgDiv.addEventListener('mouseenter', function () {
     uploadBtn.style.display = "block";
 });
- 
-imgDiv.addEventListener('mouseleave', function(){
+
+// Hide the upload button when moving the mouse away from the profile picture
+imgDiv.addEventListener('mouseleave', function () {
     uploadBtn.style.display = "none";
 });
 
- 
-
-file.addEventListener('change', function(){
-    
+// Handle file input change event
+file.addEventListener('change', function () {
     const choosedFile = this.files[0];
 
     if (choosedFile) {
+        // Create a FileReader to read the chosen file
+        const reader = new FileReader();
 
-        const reader = new FileReader();  
-
-        reader.addEventListener('load', function(){
+        // When the file is loaded, set the image source to the file's data URL
+        reader.addEventListener('load', function () {
             img.setAttribute('src', reader.result);
         });
 
+        // Read the file as a data URL
         reader.readAsDataURL(choosedFile);
-
-      
     }
 });
- function printFunction() {
-            var divContents = document.getElementById("DIV_ID").innerHTML;
-            var prnt = window.open('', '', 'height=500, width=500');
-             prnt .document.write(divContents);
-            prnt .document.close();
-            prnt .print();
+
+// Function to print the content of the page
+function printFunction() {
+    var divContents = document.body.innerHTML;
+    var prnt = window.open('', '', 'height=500, width=500');
+    
+    // Write the page content to the print window and trigger the print dialog
+    prnt.document.write(divContents);
+    prnt.document.close();
+    prnt.print();
 }
